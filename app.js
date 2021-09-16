de93aaed00f1c6e94f43d43cc90a23850f1442c9
Kenzi Nielsen <86693696+kenzinielsen@users.noreply.github.com>
2021-09-16 11:06:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+var message = 'Hello Node!';
+
+var sum = 5 + 3;
+
+console.log(message);
+console.log(sum);
