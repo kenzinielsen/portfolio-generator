14bdd41a862f7385ea4664bbe2703560da636bb6
kenzinielsen <kenzinielsen@kenzis-air.lan>
2021-09-20 19:38:10 -0600

    app.js and new folder

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const [name, github] = profileDataArgs;
 
-  console.log('================');
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
-=======
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
->>>>>>> 68c604f18d56ece0fa40f29ae4c277a8a5870dc4
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
